562b63a3e0bcfb0f35083268562e6d920cc1834f
polkadot21 <saurov.ev@gmail.com>
2023-12-16 03:06:31 +0300

    refactor main script & dockerfile cmd

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-from pyhere import here
-import sys
-from mangum import Mangum
+from services.app import app
+import uvicorn
 
-sys.path.append(str(here().resolve()))
-from services import app
-
-
-def handler(event, context):
-    "custom handler to receive userid globally"
-    asgi_handler = Mangum(app, lifespan="off")
-    response = asgi_handler(
-        event, context
-    )  # Call the instance with the event arguments
-
-    return response
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
