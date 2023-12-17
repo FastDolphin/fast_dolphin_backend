a2ba398eeda7a9ccd5894a7c939f58efb87f1c76
polkadot21 <saurov.ev@gmail.com>
2023-12-17 04:15:53 +0300

    add put metadata endpoint

--- services/routers/allowed.py
+++ services/routers/allowed.py
@@ -3,7 +3,10 @@ from typing import List, Dict, Any
 from dotenv import dotenv_values
 from fastapi import APIRouter, Request, Response, status
 from datetime import datetime
-from model import APIKey, APIKeyResponse, RouterOutput
+
+from fastapi.encoders import jsonable_encoder
+
+from model import APIKey, APIKeyResponse, RouterOutput, APIKeyMetadata
 from ..auth import is_admin_user
 
 logging.basicConfig(
@@ -48,4 +51,42 @@ async def is_api_key_allowed(
                 output.StatusMessage = "Success"
                 output.Resources.append(api_key_response)
                 output.ErrorMessage = ""
+    return output
+
+
+@router.put("/metadata", response_model=RouterOutput)
+async def is_api_key_allowed(
+    request: Request, api_key: str, tg_id: int, response: Response
+) -> RouterOutput:
+    output = RouterOutput(StatusMessage="Failure")
+    authorization_collection = request.app.database["authorization_collection"]
+    all_existing_keys_of_this_user: List[Dict[str, Any]] = list(
+        authorization_collection.find({"ApiKey": api_key})
+    )
+    if all_existing_keys_of_this_user:
+        found_keys: List[APIKey] = [
+            APIKey(**found_key) for found_key in all_existing_keys_of_this_user
+        ]
+        for key in found_keys:
+            if key.ApiKey == api_key and key.ExpiresAt > datetime.now().isoformat():
+                old_key: APIKey = key
+                key.Metadata.TgId = tg_id
+
+                encoded_updated_api_key = jsonable_encoder(key)
+                encoded_existing_api_key = jsonable_encoder(old_key)
+
+                result = authorization_collection.replace_one(
+                    encoded_existing_api_key, encoded_updated_api_key
+                )
+                if result.matched_count == 0:
+                    output.ErrorMessage = "No match for API Key."
+                    response.status_code = status.HTTP_409_CONFLICT
+                elif result.modified_count == 0:
+                    output.ErrorMessage = "API key was not modified"
+                    response.status_code = status.HTTP_409_CONFLICT
+                else:
+                    response.status_code = status.HTTP_200_OK
+                    output.StatusMessage = "Success"
+                    output.Resources.append(key.Metadata)
+                    output.ErrorMessage = ""
     return output
